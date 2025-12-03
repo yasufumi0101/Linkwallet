edda07eea0b932dbee334ce1eb8e27cf83afeae7
yasuno-N5j <yasuno@rokko-jitsugyo.co.jp>
2025-12-03 14:27:43 +0900

    コンフリクト修正

--- my-app/src/app/home/page.tsx
+++ my-app/src/app/home/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <div className="min-h-screen bg-[#E6FAF4] flex flex-col items-center">
       {/* 上の通知アイコンとかヘッダーはここに置く */}
       <div className="mt-10 mb-4 w-full flex justify-end pr-10 text-gray-500">
-        <span className="material-symbols-outlined !text-4xl">notifications</span>
+        <span className="material-symbols-outlined text-4xl!">notifications</span>
       </div>
 
       <TravelCardCarousel />
